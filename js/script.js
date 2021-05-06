9f49e87d403bedc6229cdf68cb04051157a4cee9
MyUsername <simo.3190@gmail.com>
2021-05-06 19:26:03 +0200

    todolist da finire e riguardare

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const app = new Vue({
+   el:'#app',
+
+   data:{
+    alertAcrive: false,
+
+    todos:[
+      {
+        todo:'Ripassa HTML/CSS',
+        checked: false,
+      },
+      {
+        todo:'Studiare Vue',
+        checked: false,
+      },
+      {
+        todo:'Amare Vue',
+        checked: false,
+      },
+      {
+        todo:'Alzarsi presto la mattina per ripassa',
+        checked: false,
+      },
+
+    ]
+
+
+
+
+
+
+   },
+
+   methods:{
+
+
+
+
+
+
+   }
+
+
+})
